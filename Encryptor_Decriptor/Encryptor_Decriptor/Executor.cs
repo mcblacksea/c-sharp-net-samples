4228859ef86c3507725d6dc61d6f291aaed62d9e
aodegov <a.odegov@svitla.com>
2020-11-30 10:18:40 +0200

    Added Encryptor / Decryptor project

---
+++ Encryptor_Decriptor/Encryptor_Decriptor/Executor.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encryptor_Decriptor
+{
+    public class Executor
+    {
+        private const string PasswordHash = "q%SV3GGb8rg3N$mF";
+        private const string SaltKey = "DyfJRt@u8s7Z^*uv";
+        private const string ViKey = "!I#!fus8k1L^@0l5";
+
+        public static string Encrypt(string plainText)
+        {
+            byte[] plainTextBytes = Encoding.UTF8.GetBytes(plainText);
+
+            byte[] keyBytes = new Rfc2898DeriveBytes(PasswordHash, Encoding.ASCII.GetBytes(SaltKey)).GetBytes(256 / 8);
+            var symmetricKey = new RijndaelManaged() { Mode = CipherMode.CBC, Padding = PaddingMode.Zeros };
+            var encryptor = symmetricKey.CreateEncryptor(keyBytes, Encoding.ASCII.GetBytes(ViKey));
+
+            byte[] cipherTextBytes;
+
+            using (var memoryStream = new MemoryStream())
+            {
+                using (var cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write))
+                {
+                    cryptoStream.Write(plainTextBytes, 0, plainTextBytes.Length);
+                    cryptoStream.FlushFinalBlock();
+                    cipherTextBytes = memoryStream.ToArray();
+                    cryptoStream.Close();
+                }
+                memoryStream.Close();
+            }
+            return Convert.ToBase64String(cipherTextBytes);
+        }
+
+        public static string Decrypt(string encryptedText)
+        {
+            byte[] cipherTextBytes = Convert.FromBase64String(encryptedText);
+            byte[] keyBytes = new Rfc2898DeriveBytes(PasswordHash, Encoding.ASCII.GetBytes(SaltKey)).GetBytes(256 / 8);
+            var symmetricKey = new RijndaelManaged() { Mode = CipherMode.CBC, Padding = PaddingMode.None };
+
+            var decryptor = symmetricKey.CreateDecryptor(keyBytes, Encoding.ASCII.GetBytes(ViKey));
+            var memoryStream = new MemoryStream(cipherTextBytes);
+            var cryptoStream = new CryptoStream(memoryStream, decryptor, CryptoStreamMode.Read);
+            byte[] plainTextBytes = new byte[cipherTextBytes.Length];
+
+            int decryptedByteCount = cryptoStream.Read(plainTextBytes, 0, plainTextBytes.Length);
+            memoryStream.Close();
+            cryptoStream.Close();
+            return Encoding.UTF8.GetString(plainTextBytes, 0, decryptedByteCount).TrimEnd("\0".ToCharArray());
+        }
+    }
+}
---
+++ Encryptor_Decriptor/Encryptor_Decriptor/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encryptor_Decriptor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string password_before = "", password_after = "", method = "", exit_str ="";
+
+            while (true) // Loop indefinitely
+            {
+                Console.WriteLine();
+
+                Console.WriteLine("Enter your password:");
+
+                password_before = Console.ReadLine(); // Get password string from user
+
+                Console.WriteLine("Select the method: \r\nEncrypt, press 'e' / or Decript, press 'd':");
+                method = Console.ReadLine(); // Get method
+
+                if (method == "e") // Check string
+                {
+                    password_after = Executor.Encrypt(password_before);
+                }
+                else if (method == "d")
+                {
+                    password_after = Executor.Decrypt(password_before);
+                }
+                else
+                {
+                    Console.WriteLine("The wrong key was entered...\r\nPlease try again...\r\n");
+                    continue;
+                }
+
+                Console.WriteLine(new string('-', 50));
+
+                Console.WriteLine("Your password is: \r\n{0}", password_after);
+
+                Console.WriteLine(new string('-', 50));
+
+                Console.WriteLine();
+
+                Console.WriteLine("To exit press '{0}' / To continue press any other key", "e");
+
+                exit_str = Console.ReadLine();
+
+                if (exit_str == "e")
+                {
+                    break;
+                }
+
+            }
+        }
+    }
+}
