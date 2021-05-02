df1e76f62ab65f35bab56bf6e872a02af97f48e7
anton <altecsyst@gmail.com>
2021-05-02 20:57:53 +0300

    initialized CameraBoy project on .NET 5

---
+++ CameraBoy/CameraBoy.Processor/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CameraBoy.Processor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("CameraBoy v.0.0.1");
+            Console.ReadKey();
+        }
+    }
+}
