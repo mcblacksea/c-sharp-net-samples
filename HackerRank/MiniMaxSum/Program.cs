c1e0c95e6a294b099cc797cfe71be2f2a5fa8578
anton <altecsyst@gmail.com>
2023-03-10 10:29:03 +0200

    updated program cs

--- HackerRank/MiniMaxSum/Program.cs
+++ HackerRank/MiniMaxSum/Program.cs
@@ -21,25 +21,61 @@ class Result
      * The function accepts INTEGER_ARRAY arr as parameter.
      */
 
-    public static void miniMaxSum(List<long> arr)
+    public static void miniMaxSum(List<List<int>> data, List<int> arr)
     {
-        var distinctValues = arr.Distinct();
+        List<int> result = new List<int>();
+
+        foreach (var query in arr)
+        {
+            var inCacheCurrent = 0;
+            var inCacheTotal = 0;
+
+            foreach (var lst in data)
+            {
+                var current = 0;
+                var previous = 0;
+                inCacheCurrent = 0;
+
+                foreach (var item in lst)
+                {
+                    current = item;
+                    if(previous == current)
+                    {
+                        continue;
+                    }
+
+                    if(previous + current > query)
+                    {
+                        inCacheCurrent += 1;
+                    }
+
+                    previous = current;
+                }
+                inCacheTotal += inCacheCurrent;
+            }
+
+            result.Add(inCacheTotal);
+            inCacheTotal = 0;
+        }
 
 
-        if (distinctValues.Count() <= 1)
-        {
-            var zeroNumber = arr[0];
-            var sum = arr.Sum() - zeroNumber;
-            Console.WriteLine($"{sum} {sum}");
-        }
-        else
-        {
-            var minNumber = arr.Min();
-            var maxNumber = arr.Max();
-            var minArr = arr.Where(v => v != maxNumber).ToList().Sum();
-            var maxArr = arr.Where(v => v != minNumber).ToList().Sum();
-            Console.WriteLine($"{minArr} {maxArr}");
-        }
+        //var distinctValues = arr.Distinct();
+
+
+        //if (distinctValues.Count() <= 1)
+        //{
+        //    var zeroNumber = arr[0];
+        //    var sum = arr.Sum() - zeroNumber;
+        //    Console.WriteLine($"{sum} {sum}");
+        //}
+        //else
+        //{
+        //    var minNumber = arr.Min();
+        //    var maxNumber = arr.Max();
+        //    var minArr = arr.Where(v => v != maxNumber).ToList().Sum();
+        //    var maxArr = arr.Where(v => v != minNumber).ToList().Sum();
+        //    Console.WriteLine($"{minArr} {maxArr}");
+        //}
     }
 
 }
@@ -48,15 +84,26 @@ class Solution
 {
     public static void Main(string[] args)
     {
-        List<long> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt64(arrTemp)).ToList<long>();
+        //List<long> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt64(arrTemp)).ToList<long>();
 
-        var notInRangeNumber = arr.Where(v => v < 1 || v > Math.Pow(10, 9)).Count();
-        if (notInRangeNumber > 0)
-        {
-            Console.WriteLine("array contains number, which is not in range: 1<=arr[i]<=10^9pow");
-            return;
-        }
-        Result.miniMaxSum(arr);
+        //var notInRangeNumber = arr.Where(v => v < 1 || v > Math.Pow(10, 9)).Count();
+        //if (notInRangeNumber > 0)
+        //{
+        //    Console.WriteLine("array contains number, which is not in range: 1<=arr[i]<=10^9pow");
+        //    return;
+        //}
+        Result.miniMaxSum(
+            new List<List<int>>() 
+            { 
+                 new List<int>() { 12, 11 },
+                 new List<int>() { 22, 21 },
+                 new List<int>() { 21, 32 },
+                 new List<int>() { 21, 42 },
+            },
+            new List<int>()
+            {
+                50, 40
+            });
     }
 }
 /**
